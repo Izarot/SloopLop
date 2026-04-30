f04b67c1513d8cc1bbacf0a6d41c3e5063d52f96
Izarot <sharananandanaraizarot@gmail.com>
2026-04-30 12:44:55 +0530

    Update app.js

--- ai/app.js
+++ ai/app.js
@@ -2,7 +2,11 @@ import { Memory } from "../brain/memory.js";
 import { Mood } from "../brain/mood.js";
 import { setupInput } from "./input.js";
 
-const memory = new Memory();
-const mood = new Mood();
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("APP LOADED");
 
-setupInput(memory, mood);
+  const memory = new Memory();
+  const mood = new Mood();
+
+  setupInput(memory, mood);
+});
